85074a300fcd5a2e8237912d9ac2f3f344fcea20
MIKNSJ <sculetoncode@gmail.com>
2024-08-24 21:31:37 -0700

    init basic crud template

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 from typing import Union
-
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static", html = True), name = "static")
 
 
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
+async def read_root():
+    return {"Root": "Success"}
+
+@app.get("/test")
+async def read_test():
+    return {"Test": "Success"}
 
 
 @app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
+async def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
